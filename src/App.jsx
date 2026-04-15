90963fd99876af4e41104f4f21c0c12cc1dd7530
fr3dsz <luisfrederickconda@gmail.com>
2026-04-15 12:58:21 +0800

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container mx-auto max-w-7xl">
+      {/* navbar */}
+      {/* hero */}
+      {/* about */}
+      {/* projects */}
+      {/* experience */}
+      {/* testimonial */}
+      {/* contact */}
+      {/* footer */}
+    </div>
+  );
+};
+
+export default App;
