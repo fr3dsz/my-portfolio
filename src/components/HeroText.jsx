32f12d4ed15e2bd449c3da887fe765ebedee5585
fr3dsz <luisfrederickconda@gmail.com>
2026-04-17 21:55:13 +0800

    Desktop View in HeroText.jsx

---
+++ src/components/HeroText.jsx
@@ -0,0 +1,24 @@
+import { FlipWords } from "./FlipWords";
+
+const HeroText = () => {
+  return (
+    <div className="z-10 mt-20 text-center md:mt-40 md:text-left rounded-3xl bg-clip-text">
+      {/* Desktop View */}
+      <div className="flex-col hidden md:flex c-space">
+        <h1 className="text-4xl font-medium">Hi I'm Luis</h1>
+        <div className="flex flex-col items-start">
+          <p className="text-5xl font-medium text-neutral-300">
+            A Developer <br /> Dedicated to Crafting
+          </p>
+          <div>
+            <FlipWords words={["Secure", "Modern", "Scalable"]} className="font-black text-white text-8xl" />
+          </div>
+          <p className="text-4xl font-medium text-neutral-300">Web Solutions</p>
+        </div>
+      </div>
+      {/* Mobile View */}
+    </div>
+  );
+};
+
+export default HeroText;
