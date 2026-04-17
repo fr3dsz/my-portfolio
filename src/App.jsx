2505560cac5f9816ffe86e21f5798763da557f07
fr3dsz <luisfrederickconda@gmail.com>
2026-04-17 21:24:22 +0800

    Navbar Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "./sections/Navbar";
 
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl">
-      {/* navbar */}
+      <Navbar />
       {/* hero */}
       {/* about */}
       {/* projects */}
